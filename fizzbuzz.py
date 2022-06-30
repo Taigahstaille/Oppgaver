7a3121845f6e6383f7ec01f4d3284dc907a81d54
JanVidar <jvida-ja@online.no>
2022-06-30 20:37:40 +0200

    fizzbuz

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+import random
+import math
+
+number=random.randint(1,100)
+print (number)
+
+    if (number % 3==0):
+    print("FiZZ")
+    if (number % 5==0):
+    print("Buzz")
+    if (number % 3==0) and (number % 5==0):
+    print("FiZZBuzz!")
+    
